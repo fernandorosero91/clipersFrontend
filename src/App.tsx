a0a918def84045004b4f357b4b1d085ff7993967
Fernando Rosero <elier.rosero@campusucc.edu.co>
2025-10-31 10:26:10 -0500

    arreglo de errores de vistas

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-	return (
-		<div className="min-h-screen flex bg-black text-white items-center justify-center text-2xl font-bold text-center">
-			Hello Vite + React + TailwindCSS!
-		</div>
-	);
-};
-
-export default App;
